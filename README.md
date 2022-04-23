9a0d5b08782b35cd368c519180c533c04ebf168b
rojego <rjerez@gmail.com>
2022-04-22 20:15:26 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Prueba Viajes Chile Enprendedor Digital G3
+Instalacion de Boostrap version 5.1.1
+Instalacion de Font Awesome
+Prueba conexion git
+ls
---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+/* Mostrar el tooltip del botón "Enviar" */
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
